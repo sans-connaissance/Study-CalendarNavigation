a43f79b044dc14ac8a264a70ed88bcf4bc9a7146
sans-connaissance <davemalicke@gmail.com>
2022-06-03 20:25:34 -0400

    Initial Commit

---
+++ Study-CalendarNavigation/Study_CalendarNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_CalendarNavigationApp.swift
+//  Study-CalendarNavigation
+//
+//  Created by David Malicke on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Study_CalendarNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
