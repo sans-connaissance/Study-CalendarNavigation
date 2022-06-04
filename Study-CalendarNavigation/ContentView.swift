59e7b8ffbe56114bf5e011c283a3243ed23cb7eb
sans-connaissance <davemalicke@gmail.com>
2022-06-03 21:07:47 -0400

    ADD initial top layout

--- Study-CalendarNavigation/ContentView.swift
+++ Study-CalendarNavigation/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack(alignment: .center) {
+                    Text("S")
+                    Text("M")
+                    Text("T")
+                    Text("W")
+                    Text("T")
+                    Text("F")
+                    Text("S")
+                }
+                HStack(alignment:.center, spacing: 10) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                    Text("7")
+                }
+                Text("Saturday June 3, 2022")
+                Divider()
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct DayView: View {
+    
+    var body: some View {
+        
+        
     }
 }
 
